0ac0327a26c7ef112549a252f66fe580e6cf006e
JANG-WON-JUN <jangwonjun0509@gmail.com>
2023-09-24 00:40:47 +0900

    💄 [Design] : 메인 화면 추가

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,40 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router';
+import { useRouter, RouterView } from 'vue-router';
+import loginStore from './stores/login-store';
+
+const useLoginStore = loginStore();
+const router = useRouter();
+
+const logout = function () {
+  useLoginStore.logout();
+  router.push({ name: 'main' });
+};
+
+const portfolio = () => {
+  alert('포트폴리오 메뉴 준비중입니다.');
+};
 </script>
 
 <template>
+  <el-row :gutter="10" align="middle" class="header">
+    <el-col :span="4">
+      <div class="grid-content ep-bg-purple" />
+      <img src="" alt="스포포 로고" @click="router.push({ name: 'main' })" />
+    </el-col>
+    <el-col :span="4" class="txt-center">
+      <div class="grid-content ep-bg-purple-light" />
+      <el-link :underline="false" @click="portfolio">포트폴리오</el-link>
+    </el-col>
+    <el-col :span="4" class="txt-center"><div class="grid-content ep-bg-purple" /> </el-col>
+    <el-col :span="4" class="txt-center"><div class="grid-content ep-bg-purple-light" /> </el-col>
+    <el-col :span="4" class="txt-center"><div class="grid-content ep-bg-purple" /> </el-col>
+    <el-col class="txt-center" :span="4" v-if="useLoginStore.isAuthedMember">
+      <span>{{ useLoginStore.getMember.name }}님 안녕하세요</span>
+      <el-link :underline="false" @click="logout()">로그아웃</el-link>
+    </el-col>
+    <el-col :span="4" v-else class="txt-center">
+      <el-link :underline="false" @click="router.push({ name: 'login' })">로그인</el-link>
+    </el-col>
+  </el-row>
   <RouterView />
 </template>
--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,38 +1,23 @@
 <script setup>
 import { useRouter } from 'vue-router';
-
-// ref란 vue에서 컴포넌트 또는 DOM에 접근하기 위해 사용하는 속성이다.
-
-// const email = ref('');
 const router = useRouter();
-
-const login = function () {
-  router.push({ name: 'login' });
-};
-
-// 서비스로 나누기
-const callPortfolioServer = async () => {
-  const portfolioService = (await import('../components/services/portfolio-service')).default;
-  portfolioService.testCall();
-};
-
-const callStockServer = async () => {
-  const stockService = (await import('../components/services/stock-service')).default;
-  stockService.testCall();
-};
-
-const callAuthServer = async () => {
-  const authService = (await import('../components/services/auth-service')).default;
-  authService.testCall();
-};
 </script>
 
 <template>
-  <h1>메인화면 입니다</h1>
-  <button @click="login">로그인페이지로 이동!</button> <br />
-  <button @click="callPortfolioServer">포트폴리오 서버 호출</button> <br />
-  <button @click="callStockServer">주식 서버 호출</button> <br />
-  <button @click="callAuthServer">인증 서버 호출</button> <br />
+  <div class="main-container">
+    <el-row :gutter="24">
+      <el-col>
+        <img src="../assets/images/kakao_logo.png" />
+      </el-col>
+    </el-row>
+    <el-row :gutter="24">
+      <el-col>
+        <el-button class="login-btn" color="#DBE2EF" round @click="router.push({ name: 'login' })">
+          로그인
+        </el-button>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 
 <style>
