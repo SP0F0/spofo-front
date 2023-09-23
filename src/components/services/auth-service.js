0b69d7668e7ce05eadaa8caf89b9dfc36393dad6
JANG-WON-JUN <jangwonjun0509@gmail.com>
2023-09-24 00:37:56 +0900

    ✨ [Feat] : 인증 서버 연결 API 추가

--- src/components/services/auth-service.js
+++ src/components/services/auth-service.js
@@ -1,12 +1,13 @@
 import axios from '../common/axios-config';
-import { useCookies } from 'vue3-cookies';
 
 class AuthService {
   authServer = import.meta.env.VITE_AUTH_SERVER;
 
-  testCall() {
-    console.log('인증 서버 테스트 API 호출');
-    axios.get(this.authServer + '/test').then((response) => console.log(response));
+  // API 연결 필요
+  addMember(loggedInMemberInfo) {
+    axios
+      .put(this.authServer + '/auth/members', loggedInMemberInfo)
+      .then((response) => console.log(response));
   }
 }
 
