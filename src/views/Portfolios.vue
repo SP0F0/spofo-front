9674e5fa7b8d70124a0204c69745198b81e3aa41
JANG-WON-JUN <jangwonjun0509@gmail.com>
2023-09-24 14:47:54 +0900

    💄 [Design] : 포트폴리오 관련 vue 추가

---
+++ src/views/Portfolios.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { ref, watch, onMounted } from 'vue';
+import { useRouter, useRoute } from 'vue-router';
+import portfolioService from '../components/services/portfolio-service';
+
+onMounted(() => {
+  console.log("'onMount");
+  portfolioService.
+});
+</script>
+
+<template></template>
+
+<style scoped></style>
