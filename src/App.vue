6a5880792e55cfe61ec254c1bfa45f5ab2d97ede
JANG-WON-JUN <jangwonjun0509@gmail.com>
2023-09-24 12:24:58 +0900

    💄 [Design] : 로그인 후 프로필 선택 시 드롭다운 기능 추가

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@ import type { DropdownInstance } from 'element-plus';
 
 const useLoginStore = loginStore();
 const router = useRouter();
-const dropdown1 = ref<DropdownInstance>();
+const memberDropdown = ref<DropdownInstance>();
 
 const logout = function () {
   useLoginStore.logout();
@@ -22,41 +22,40 @@ const test = () => {
 };
 
 function showClick() {
-  if (!dropdown1.value) return;
-  dropdown1.value.handleOpen();
+  if (!memberDropdown.value) return;
+  memberDropdown.value.handleOpen();
 }
 </script>
 
 <template>
-  <div style="margin: 15px">
-    <el-button @click="showClick">show</el-button>
-  </div>
-  <el-dropdown ref="dropdown1" trigger="contextmenu" style="margin-right: 30px">
-    <span class="el-dropdown-link"> Dropdown List1 </span>
-    <template #dropdown>
-      <el-dropdown-menu>
-        <el-dropdown-item>Action 1</el-dropdown-item>
-        <el-dropdown-item>Action 2</el-dropdown-item>
-        <el-dropdown-item>Action 3</el-dropdown-item>
-        <el-dropdown-item disabled>Action 4</el-dropdown-item>
-        <el-dropdown-item divided>Action 5</el-dropdown-item>
-      </el-dropdown-menu>
-    </template>
-  </el-dropdown>
   <el-row :gutter="10" align="middle" class="header">
     <el-col :span="4">
       <div class="grid-content ep-bg-purple" />
       <img src="" alt="스포포 로고" @click="router.push({ name: 'main' })" />
     </el-col>
-    <el-col :span="4" class="txt-center">
+    <el-col :span="4" class="header-content">
       <div class="grid-content ep-bg-purple-light" />
       <el-link :underline="false" @click="portfolio">포트폴리오</el-link>
     </el-col>
-    <el-col :span="4" class="txt-center"><div class="grid-content ep-bg-purple" /> </el-col>
-    <el-col :span="4" class="txt-center"><div class="grid-content ep-bg-purple-light" /> </el-col>
-    <el-col :span="4" class="txt-center"><div class="grid-content ep-bg-purple" /> </el-col>
-    <el-col class="txt-center" :span="4" v-if="useLoginStore.isAuthedMember">
-      <el-avatar :size="50" :src="useLoginStore.getMember.imagePath" @click="test" />
+    <el-col :span="4" class="header-content"><div class="grid-content ep-bg-purple" /> </el-col>
+    <el-col :span="4" class="header-content"><div class="grid-content ep-bg-purple" /> </el-col>
+    <el-col :span="4" class="header-content"><div class="grid-content ep-bg-purple" /> </el-col>
+    <el-col class="header-content" :span="4" v-if="useLoginStore.isAuthedMember">
+      <el-dropdown ref="memberDropdown" trigger="contextmenu" size="large">
+        <span class="el-dropdown-link">
+          <el-avatar :size="50" :src="useLoginStore.getMember.imagePath" @click="showClick" />
+        </span>
+        <template #dropdown>
+          <el-dropdown-menu>
+            <el-dropdown-item>Action 1</el-dropdown-item>
+            <el-dropdown-item>Action 2</el-dropdown-item>
+            <el-dropdown-item>Action 3</el-dropdown-item>
+            <el-dropdown-item divided>
+              <el-link :underline="false" @click="logout">로그아웃</el-link>
+            </el-dropdown-item>
+          </el-dropdown-menu>
+        </template>
+      </el-dropdown>
     </el-col>
     <el-col :span="4" v-else class="txt-center">
       <el-link :underline="false" @click="router.push({ name: 'login' })">로그인</el-link>
