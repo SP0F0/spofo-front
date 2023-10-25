cf7e20d302d18a235a40d18cbe906a08ca257348
Han <keg51051@gmail.com>
2023-10-25 10:18:00 +0900

    :sparkles: [Feat] : 배너, 메인 페이지 디자인 변경 및 추가

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -5,27 +5,19 @@ const router = useRouter();
 
 <template>
   <div class="main-container">
-    <el-row :gutter="24">
-      <el-col>
-        <!-- 메인 이미지가 들어갈 위치 -->
-        <div class="main-image-container">
-          <div style="width: 100%">
-            <img src="../assets/images/main_1.jpeg" style="max-width: 100%" />
-          </div>
-          <div style="width: 100%">
-            <img src="../assets/images/main_2.jpeg" style="max-width: 100%" />
-          </div>
-        </div>
-      </el-col>
-    </el-row>
-    <el-row :gutter="24">
-      <el-col>
-        <el-button class="login-btn" color="#DBE2EF" round @click="router.push({ name: 'login' })">
-          시작하기
-        </el-button>
-      </el-col>
-    </el-row>
+    <img style="width: 100vw; height: 100vh; position: fixed; right: 0%;" src="../assets/images/circular_bg.png" />
+    <div class="first-box">
+      <el-row class="main-first-text">
+        누구나 쉽게 관리하는 주식 포트폴리오
+      </el-row>
+      <img src="../assets/images/logo.png" class="main-first-logo" />
+      <el-row>
+        <el-col>
+          <el-button class="login-btn" color="#120064" round @click="router.push({ name: 'login' })">
+            시작하기
+          </el-button>
+        </el-col>
+      </el-row>
+    </div>
   </div>
 </template>
-
-<style></style>
