01a7a3ca195bfbd6118b7fe9ba54a1d13b90f904
Han <keg51051@gmail.com>
2023-10-31 15:56:18 +0900

    chart.js 버그 수정을 위한 초기화

--- src/views/ReturnView.vue
+++ src/views/ReturnView.vue
@@ -1,52 +1,6 @@
-<script>
-import {
-    Chart as ChartJS,
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend
-} from 'chart.js'
-import { Line } from 'vue-chartjs'
-
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip,
-  Legend
-)
-
-export default {
-    name: 'LineChart',
-    components: { Line },
-    data() {
-        return {
-            chartData: {
-                labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-                datasets: [
-                    {
-                        label: 'Data One',
-                        backgroundColor: '#120064',
-                        data: [40, 39, 10, 40, 39, 80, 40]
-                    }
-                ]
-            },
-            chartOptions: {
-                responsive: true,
-                maintainAspectRatio: false
-            }
-        }
-    }
-}
+<script setup lang="ts">
 </script>
 
 <template>
-    <Line class="return-chart" :options="chartOptions" :data="chartData" />
+
 </template>
-  
-  
