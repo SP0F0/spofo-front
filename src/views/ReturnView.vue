9686b5d0dd32d60bd73e25022b7f41883ce28122
Han <keg51051@gmail.com>
2023-11-03 09:44:28 +0900

    :sparkles: [Feat] : 누적 수익률 차트 임시 구현, 내비게이션 바 간격 축소

--- src/views/ReturnView.vue
+++ src/views/ReturnView.vue
@@ -1,3 +1,43 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+} from 'chart.js'
+import { Line } from 'vue-chartjs'
 
-<template></template>
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+)
+
+const data = {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    datasets: [
+        {
+            label: 'Data One',
+            backgroundColor: '#f87979',
+            data: [40, 39, 10, 40, 39, 80, 40]
+        }
+    ]
+}
+
+const options = {
+    responsive: true,
+    maintainAspectRatio: false
+}
+</script>
+
+<template>
+    <Line :data="data" :options="options" />
+</template>
