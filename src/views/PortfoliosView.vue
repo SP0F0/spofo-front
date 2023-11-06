100714809a0b39b1e6d8a332ab949d36e1d2662a
JANG-WON-JUN <jangwonjun0509@gmail.com>
2023-11-06 14:40:45 +0900

    ✨ [Feat] : 포트폴리오 조회 시 필터링 기능 추가

--- src/views/PortfoliosView.vue
+++ src/views/PortfoliosView.vue
@@ -7,31 +7,35 @@ import type { PortfolioSimple } from '@/components/models/portfolio-simple';
 import { Plus } from '@element-plus/icons-vue';
 import PortfolioTag from '@/components/common/PortfolioTag.vue';
 import { PortfolioModify } from '@/components/models/portfolio-modify';
-import { Select, CloseBold } from '@element-plus/icons-vue'
+import { Select, CloseBold } from '@element-plus/icons-vue';
 
 const router = useRouter();
-const filterOption = ref('전체');
+const filterOption = ref('');
 const portfoliosSummary = ref(new PortfoliosSummary());
 const portfolioSimples = ref<PortfolioSimple[]>();
 
 onMounted(() => {
-  getPortfoliosTotal();
-  getPortfolioSimples();
+  getPortfoliosTotal('');
+  getPortfolioSimples('');
 });
 
-const getPortfolioSimples = () =>
+const getPortfolioSimples = (type: string) =>
   portfolioService
-    .getPortfolioSimples()
-    .then((response) => {
-      (portfolioSimples.value = response.data)
-      console.log(response.data);
+    .getPortfolioSimples({
+      type: type
     })
+    .then((response) => (portfolioSimples.value = response.data))
     .catch((error) => console.log(error));
 
-const getPortfoliosTotal = () =>
+const getPortfoliosTotal = (type: string) =>
   portfolioService
-    .getPortfoliosTotal()
-    .then((response) => (portfoliosSummary.value = response.data))
+    .getPortfoliosTotal({
+      type: type
+    })
+    .then((response) => {
+      portfoliosSummary.value = response.data;
+      console.log(response.data);
+    })
     .catch((error) => console.log(error));
 
 const viewPortfolio = (portfolioId: number) => {
@@ -54,14 +58,15 @@ const switchInclude = (portfolio: PortfolioSimple) => {
   portfolioService
     .modifyPortfolio(modifiedPortfolio)
     .then((response) => {
-      getPortfoliosTotal();
-      getPortfolioSimples();
+      getPortfoliosTotal('');
+      getPortfolioSimples('');
     })
     .catch((error) => console.log(error));
 };
 
 const changeFilterOption = () => {
-  // API를 호출하여 회원 1명의 포트폴리오를 필터링하여 조회한다.
+  getPortfoliosTotal(filterOption.value);
+  getPortfolioSimples(filterOption.value);
 };
 </script>
 
@@ -127,12 +132,17 @@ const changeFilterOption = () => {
           <div class="card-header">
             <el-row align="middle">
               <el-col :span="24">
-                <el-select v-model="filterOption" class="m-2" placeholder="포트폴리오 구분" size="large"
-                  @change="changeFilterOption">
-                  <el-option key="전체" label="전체" value="전체" />
-                  <el-option key="실제" label="실제" value="실제" />
-                  <el-option key="모의" label="모의" value="모의" />
-                  <el-option key="계좌 연동" label="계좌 연동" value="계좌 연동" />
+                <el-select
+                  v-model="filterOption"
+                  class="m-2"
+                  placeholder="포트폴리오 구분"
+                  size="large"
+                  @change="changeFilterOption"
+                >
+                  <el-option key="전체" label="전체" value="" />
+                  <el-option key="실제" label="실제" value="REAL" />
+                  <el-option key="모의" label="모의" value="FAKE" />
+                  <el-option key="계좌 연동" label="계좌 연동" value="LINK" />
                 </el-select>
               </el-col>
             </el-row>
@@ -151,12 +161,22 @@ const changeFilterOption = () => {
                   </el-link>
                 </el-col>
                 <el-col :span="4">
-                  <el-switch v-model="item.includeType" size="large"
-                    style="float: right; --el-switch-on-color: #120064; --el-switch-off-color: #0065B8"
-                    @click="switchInclude(item)" inline-prompt :active-icon="Select" :inactive-icon="CloseBold" />
+                  <el-switch
+                    v-model="item.includeType"
+                    size="large"
+                    style="
+                      float: right;
+                      --el-switch-on-color: #120064;
+                      --el-switch-off-color: #0065b8;
+                    "
+                    @click="switchInclude(item)"
+                    inline-prompt
+                    :active-icon="Select"
+                    :inactive-icon="CloseBold"
+                  />
                 </el-col>
               </el-row>
-              <el-row class="stock-card-content" :align="'center'">
+              <el-row class="stock-card-content" align="middle">
                 <el-col :span="3"> 자산가치&nbsp; </el-col>
                 <el-col :span="21"> {{ item.totalAsset.toLocaleString() }}원 </el-col>
                 <el-col :span="3"> 수익률&nbsp; </el-col>
